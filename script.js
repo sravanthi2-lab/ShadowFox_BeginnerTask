630e2f59d878875770f0bc62f48ab06c7d61d6c7
sravanthi2-lab <245122749301@mvsrec.edu.in>
2025-07-19 23:09:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission handler (non-functional backend)
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Message sent! (This is just a demo)");
+});
